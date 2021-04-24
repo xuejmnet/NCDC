ad7605da666e1328397aa33412de0f33eca071e7
xuejmnet <326308290@qq.com>
2021-04-24 09:42:41 +0800

    112

---
+++ src/sharding/ShardingConnector.ShardingCommon/Core/Strategy/KeyGen/UUIDShardingKeyGenerator.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using ShardingConnector.ShardingApi.Spi.KeyGen;
+
+namespace ShardingConnector.ShardingCommon.Core.Strategy.KeyGen
+{
+/*
+* @Author: xjm
+* @Description:
+* @Date: Friday, 23 April 2021 21:41:54
+* @Email: 326308290@qq.com
+*/
+    public sealed class UUIDShardingKeyGenerator:IShardingKeyGenerator
+    {
+        private IDictionary<string, object> _properties = new Dictionary<string, object>();
+        public string GetAlgorithmType()
+        {
+            return "UUID";
+        }
+
+        public IDictionary<string, object> GetProperties()
+        {
+            return _properties;
+        }
+
+        public void SetProperties(IDictionary<string, object> properties)
+        {
+            this._properties = properties;
+        }
+
+        public IComparable GenerateKey()
+        {
+            return Guid.NewGuid().ToString("n");
+        }
+    }
+}
