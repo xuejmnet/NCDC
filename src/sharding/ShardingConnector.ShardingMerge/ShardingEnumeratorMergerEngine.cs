bd84c09510af5aa042f57b8be546e084632f11dd
xuejiaming <326308290@qq.com>
2021-05-08 09:12:40 +0800

    compile succeed

--- src/sharding/ShardingConnector.ShardingMerge/ShardingEnumeratorMergerEngine.cs
+++ src/sharding/ShardingConnector.ShardingMerge/ShardingEnumeratorMergerEngine.cs
@@ -7,24 +7,27 @@ using ShardingConnector.ParserBinder.Command;
 using ShardingConnector.ParserBinder.Command.DML;
 using ShardingConnector.ShardingCommon.Core.Rule;
 using ShardingConnector.ShardingMerge.DAL;
+using ShardingConnector.ShardingMerge.DQL;
 using ShardingConnector.Spi.DataBase.DataBaseType;
 
 namespace ShardingConnector.ShardingMerge
 {
-/*
-* @Author: xjm
-* @Description:
-* @Date: Wednesday, 05 May 2021 21:16:33
-* @Email: 326308290@qq.com
-*/
-    public class ShardingEnumeratorMergerEngine:IResultMergerEngine<ShardingRule>
+    /*
+    * @Author: xjm
+    * @Description:
+    * @Date: Wednesday, 05 May 2021 21:16:33
+    * @Email: 326308290@qq.com
+    */
+    public class ShardingEnumeratorMergerEngine : IResultMergerEngine<ShardingRule>
     {
         public IResultMerger NewInstance(IDatabaseType databaseType, ShardingRule rule, ConfigurationProperties properties, ISqlCommandContext<ISqlCommand> sqlCommandContext)
-        {  
-            if (sqlCommandContext is SelectCommandContext) {
-                return new ShardingDQLResultMerger(databaseType);
-            } 
-            if (sqlCommandContext.GetSqlCommand() is DALCommand) {
+        {
+            if (sqlCommandContext is SelectCommandContext)
+            {
+                return new ShardingDQLEnumeratorMerger(databaseType);
+            }
+            if (sqlCommandContext.GetSqlCommand() is DALCommand)
+            {
                 return new ShardingDALEnumeratorMerger(rule);
             }
             return new TransparentResultMerger();
